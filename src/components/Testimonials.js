153407e364c52789a39977720af6e9abb91780ad
creativegithub <140626864+creativegithub@users.noreply.github.com>
2024-07-04 10:10:18 +0530

    initial commit

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Card from './Card';
+import Card from '../Card';
 import {FiChevronLeft, FiChevronRight} from "react-icons/fi";
 
 export default function Testimonials(props) {
